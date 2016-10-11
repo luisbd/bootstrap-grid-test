bb751c9a060160206359a14785acfcc15c41af64
luisbd <luis.belmonte.diaz@gmail.com>
2016-10-11 23:04:55 +0200

    Reseting compiled files

--- Test-4_Self-hosted-BS-mixins/gulpfile.js
+++ Test-4_Self-hosted-BS-mixins/gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 
 var sassSrc = './src/styles/**/*.scss';
 var cssDir = './public/styles/non-min';
-var cssMinDir = './public/styles/min'
+var cssMinDir = './public/styles/min';
 
 gulp.task('sass', function() {
     return gulp.src(sassSrc)
